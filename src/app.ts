2874e784cad35d7c9059b2254b73916366227820
antn9x <antn9x@gmail.com>
2024-09-27 01:37:07 +0700

    fix app

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import { Constructor, System } from 'entityx-ts'
 import { Application } from 'pixi.js'
 import { actionManager } from 'pixi-action-ease'
 
-import { NodeComp } from './components/NodeComp'
-
 export const app = new Application()
 
 export function setupResolution(designedResolution = { width: 720, height: 1280 }) {
@@ -12,30 +10,6 @@ export function setupResolution(designedResolution = { width: 720, height: 1280 
   app.renderer.resize(width, height)
   // app.stage.position.y = app.renderer.height / app.renderer.resolution
   // app.stage.scale.y = -1
-}
-
-export async function addGameCanvasTo(id = 'game') {
-  await app.init({
-    antialias: true,
-    resolution: window.devicePixelRatio,
-    resizeTo: window,
-    eventFeatures: {
-      move: true,
-      /** disables the global move events which can be very expensive in large scenes */
-      globalMove: false,
-      click: true,
-      wheel: false,
-    },
-  })
-  Object.assign(app.canvas.style, {
-    width: `${window.innerWidth}px`,
-    // height: `${window.innerHeight}px`,
-    overflow: 'hidden',
-  })
-
-  const gameDiv = document.getElementById(id)
-  gameDiv.appendChild(app.canvas)
-  GameWorld.Instance.setup(NodeComp, app.stage)
 }
 
 function startGameLoop(world: GameWorld) {
