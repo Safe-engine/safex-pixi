6fe417440648b63575fb83f41185ab20f96a9e92
antn <antn@ftech.ai>
2025-02-04 09:43:21 +0700

    fix instantiate

--- src/components/BaseComponent.ts
+++ src/components/BaseComponent.ts
@@ -4,23 +4,14 @@ import { EnhancedComponent, GameWorld } from '../base'
 import { NodeComp } from './NodeComp'
 
 export class NoRenderComponentX<Props = Object, C extends Container = Container> extends EnhancedComponent<Props, NodeComp<C>> {
-  static create(data?: any) {
-    return new this(data)
-  }
 }
 
 export class ComponentX<Props = Object, C extends Container = Container> extends EnhancedComponent<Props, NodeComp<C>> {
   static hasRender = true
-  render?(data?: Props): any
-  static create(data?: any) {
-    // console.log('create', this, this.prototype['render'])
-    if (this.prototype.render) {
-      const instance = new this(data)
-      return instance.render(data)
-    }
+  render?(): this {
     const world = GameWorld.Instance
     const root = world.entities.create()
-    const comp = root.assign(new this(data))
+    const comp = root.assign(this)
     return comp
   }
 }
