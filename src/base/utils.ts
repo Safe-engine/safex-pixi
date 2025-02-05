4d0f684db31a4a4c73c954416b25e8c4f13f8f9b
antn <antn@ftech.ai>
2025-02-05 10:30:03 +0700

    add loadScene

--- src/base/utils.ts
+++ src/base/utils.ts
@@ -1,5 +1,6 @@
 import { Constructor } from 'entityx-ts';
 
+import { GameWorld } from '..';
 import { ComponentX, NoRenderComponentX } from '../components/BaseComponent';
 
 export type GetProps<T> = T extends ComponentX<infer P> ? P : T extends NoRenderComponentX<infer Q> ? Q : never;
@@ -12,4 +13,11 @@ export function instantiate<T extends ComponentX>(ComponentType: Constructor<T>,
     return instance
   }
   return instance.render()
+}
+
+export function loadScene<T extends ComponentX>(ComponentType: Constructor<T>) {
+  const world = GameWorld.Instance
+  world.entities.reset()
+  const instance = new ComponentType()
+  instance.render()
 }
